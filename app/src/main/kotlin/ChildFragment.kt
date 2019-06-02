3abbe6a73fb55331963f2ffb0228f0df8efc1698
TERA630 <tera.bcool.4475@gmail.com>
2019-06-02 22:31:09 +0900

    unifiying recycler view

--- app/src/main/kotlin/ChildFragment.kt
+++ app/src/main/kotlin/ChildFragment.kt
@@ -15,6 +15,15 @@ class ChildFragment : Fragment() {
     private lateinit var mAdaptor: ChildListAdaptor
 
     companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ChildFragment.
+         */
+        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(parent: String, _list: List<String>) =
             ChildFragment().apply {
--- app/src/main/kotlin/OriginListAdaptor.kt
+++ app/src/main/kotlin/OriginListAdaptor.kt
@@ -5,80 +5,78 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.ViewAnimator
 import kotlinx.android.synthetic.main.card_folder.view.*
 import kotlinx.android.synthetic.main.card_list.view.*
 import kotlinx.android.synthetic.main.card_list.view.rowText
 import java.util.*
 
-class OriginListAdaptor(private var mResults: MutableList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class
+OriginListAdaptor(private var mResults: MutableList<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     // ViewType 0 or except 1: item, 1: folder
     // String   "something, some1 , some 2 , ..."
     private lateinit var mHandler: OriginFragment.DeliverEventToActivity
+
+    // Lifecycle of Recycler View
+    override fun getItemCount(): Int = mResults.size
+    override fun getItemViewType(position: Int): Int = indicateViewType(mResults[position])
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val viewHolder = ViewHolderOfCell(inflater.inflate(R.layout.card_list, parent, false))
+        when (viewType) {
+            1 -> {
+                viewHolder.hasChild = true
+            }
+            else -> {
+                viewHolder.hasChild = false
+            }
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val list = mResults[position].split(",")
+        val parentString = list[0]
+        val droppedList = list.drop(1)
+        val vH = holder as ViewHolderOfCell
+        if (vH.hasChild) {
+            vH.childList = droppedList.toMutableList()
+            vH.rowView.folderIcon.visibility = View.VISIBLE
+        } else {
+            vH.rowView.folderIcon.visibility = View.GONE
+        }
+        vH.rowView.rowText.text = parentString
+        vH.rowView.rowText.setOnClickListener {
+            Log.i("test", "Text clicked")
+        }
+    }
 
     fun addResult(result: String) {
         mResults.add(0, "$result,0")
         notifyItemInserted(0)
     }
+
+    fun getResults(): MutableList<String> = mResults
+
+
     fun upDateResultList(stringArray: ArrayList<String>) {
         mResults = stringArray
         notifyDataSetChanged()
     }
-    override fun getItemCount(): Int = mResults.size
-    fun getResults(): MutableList<String> = mResults
-
-    override fun getItemViewType(position: Int): Int = indicateViewType(mResults[position])
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val list = mResults[position].split(",")
-        val parentString = list[0]
-        holder.itemView.rowText.setOnClickListener {
-            Log.i("test", "EditText clicked")
-        }
-        if (holder is ViewHolderOfFolder) {
-            holder.rowView.rowText.text = parentString
-            holder.parent = parentString
-            val droppedList = list.drop(1)
-            holder.childList = droppedList.toMutableList()
-            holder.rowView.folderIcon.setOnClickListener {
-                Log.i("test", "folder clicked ${holder.childList}")
-                mHandler.onUserInterAction(parentString, holder.childList)
-            }
-        } else if (holder is ViewHolderOfCell) {
-            holder.rowView.rowText.text = parentString
-        }
-    }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        when (viewType) {
-            1 -> {
-                return ViewHolderOfFolder(inflater.inflate(R.layout.card_folder, parent, false))
-            }
-            else -> {
-                val VH = ViewHolderOfCell(inflater.inflate(R.layout.card_list, parent, false))
-                VH.textWrapper = parent.textWrapper
-                return VH
-            }
-
-        }
-    }
-
     fun setUIHandler(_handler: OriginFragment.DeliverEventToActivity) {
         this.mHandler = _handler
     }
     // private method
 
-    private fun editParent(_view: View, parentString: String, _position: Int) {
-        if (_view is TextView) {
+    /*  private fun editParent(_view: View, parentString: String, _position: Int) {
+          if (_view is TextView) {
 
 
-        } else {
+          } else {
 
-        }
+          }
 
 
-    }
+      }*/
     private fun indicateViewType(_string: String): Int {
         val list = _string.split(",")
         return if (list.size > 2) 1 else 0 // ITEMが一つなら　ViewType:0　Itemをかえす。　そうでなければFolderをかえす。
@@ -86,11 +84,7 @@ class OriginListAdaptor(private var mResults: MutableList<String>) : RecyclerVie
 
 
     class ViewHolderOfCell(val rowView: View) : RecyclerView.ViewHolder(rowView) {
-        var textWrapper: ViewAnimator? = null
-    }
-    class ViewHolderOfFolder(val rowView: View) : RecyclerView.ViewHolder(rowView) {
-        var parent: String = ""
+        var hasChild = false
         var childList: MutableList<String> = emptyList<String>().toMutableList()
     }
-
 }
