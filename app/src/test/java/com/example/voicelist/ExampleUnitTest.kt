8c36312c0cb22d15683cedd37b223f89fdd86bff
TERA630 <tera.bcool.4475@gmail.com>
2019-06-06 22:07:16 +0900

    viewmodel made. test regex difficult

--- app/src/test/java/com/example/voicelist/ExampleUnitTest.kt
+++ app/src/test/java/com/example/voicelist/ExampleUnitTest.kt
@@ -1,7 +1,12 @@
 package com.example.voicelist
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.experimental.theories.DataPoints
+import org.junit.experimental.theories.Theories
+import org.junit.experimental.theories.Theory
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -16,14 +21,19 @@ class ExampleUnitTest {
 
 }
 
+@RunWith(Theories::class)
 class RegexTest {
     val sampleData = listOf(
         "have:origin,had,had",
         "two:origin,give,gave,given",
         "three:origin,fire,blizzard,thunder",
         "four:origin,fire,fira,figa",
-        "five"
+        "five",
+        "six:origin,Tina,Rock,Mash,Edo"
     )
+    @DataPoints
+    val dataOne = Fixture(1, listOf("had", "had"))
+    val dataTwo = Fixture(2, listOf("give", "gave", "given"))
 
     @Test
     fun testOriginRegEx() {
@@ -35,6 +45,18 @@ class RegexTest {
                 result.add(header)
             }
         }
+        assertThat(result).isEqualTo(mutableListOf("have", "two", "three", "four", "six"))
+    }
+
+    data class Fixture(val dataIndex: Int, val dataList: List<String>)
+
+
+    @Theory
+    fun testChildList(index: Int, expected: List<String>) {
+        val headAndChildCSV = sampleData[index]
+        val list = headAndChildCSV.split(",")
+        val result = list.drop(1)
+        assertThat(result).isEqualTo(expected)
     }
 
 
